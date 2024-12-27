80745e73a6691a0099860672785e1281a646be2b
K. <kylebsmall32@gmail.com>
2024-12-27 16:06:52 -0500

    checkpoint leaderboard component

---
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard',
+  imports: [],
+  templateUrl: './leaderboard.component.html',
+  styleUrl: './leaderboard.component.css'
+})
+export class LeaderboardComponent {
+
+}
